3a73c0595866f909a1d49e9ec2d7ed1ecbf3f67b
Azlan Jalil <azlanjalil543@gmail.com>
2025-11-13 13:25:04 +0500

    initial commit APIs

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <header className="bg-blue-600 p-4">
+        <h1 className="text-5xl font-bold text-white">Header</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
